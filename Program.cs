4a2823321bc22ac758a5c09d346680582cda27b7
25_1 <dzavizistur@gmial.com>
2025-04-02 12:50:17 +0300

    поправки

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@
         {
             Console.WriteLine("enter sum");
             float.TryParse(Console.ReadLine(), out float x);
-            if ( x < 100) { Console.WriteLine($"ваш счет {x * 0.05}"); }
-            else if (x > 100 & x < 200) { Console.WriteLine($"ваш счет{x*0.07}"); }
-            else { Console.WriteLine($"ваш счет{x * 0.1}"); }
+            if ( x < 100) { Console.WriteLine($"ваш счет {x + x * 0.05}"); }
+            else if (x > 100 && x < 200) { Console.WriteLine($"ваш счет{x + x*0.07}"); }
+            else { Console.WriteLine($"ваш счет{x + x * 0.1}"); }
         }
     }
 }
