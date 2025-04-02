a1da1166008fa855135ada1f17c5b228425bc596
User <User@25_1>
2025-04-02 09:16:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace N3__YP__Task_3_02._04._2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter sum");
+            float.TryParse(Console.ReadLine(), out float x);
+            if ( x < 100) { Console.WriteLine($"ваш счет {x * 0.05}"); }
+            else if (x > 100 & x < 200) { Console.WriteLine($"ваш счет{x*0.07}"); }
+            else { Console.WriteLine($"ваш счет{x * 0.1}"); }
+        }
+    }
+}
